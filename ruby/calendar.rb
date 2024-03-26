2fa4c3d4acccfbb6983746a327897c5230873514
shu <chukichi915@gmail.com>
2024-03-26 11:28:38 +0900

    カレンダーができた

---
+++ ruby/calendar.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'date'
+require 'optparse'
+
+option = {}
+opt = OptionParser.new
+opt.on('-m MONTH', Integer) do |month|
+  option[:month] = month
+end
+opt.parse!(ARGV)
+
+year = Date.today.year
+month = option[:month] || Date.today.month
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
+
+puts "#{year}年 #{month}月".center(20)
+puts '月 火 水 木 金 土 日'
+
+print '   ' * ((first_day.wday + 6) % 7)
+
+(first_day..last_day).each do |day|
+  day = if day.sunday?
+          "#{day.day.to_s.rjust(2)}\n"
+        else
+          "#{day.day.to_s.rjust(2)} "
+        end
+  print day
+end
