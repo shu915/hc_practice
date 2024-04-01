7d014ff414c5a17971f0dcb777729f543731f5a8
shu <chukichi915@gmail.com>
2024-04-01 16:11:22 +0900

    rubocopを通した

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
+
 SCORE_MAPPING = {
   -4 => 'コンドル',
   -3 => 'アルバトロス',
   -2 => 'イーグル',
   -1 => 'バーディ',
   0 => 'パー',
-  1 => 'ボギー',
+  1 => 'ボギー'
 }
 
 def calc_golf_score(pars, player_score)
@@ -14,19 +15,18 @@ def calc_golf_score(pars, player_score)
     diff = score - par
 
     result << case
-      ##ホールインワンよりコンドルを優先する
-      when score == 1 && diff != -4
-        'ホールインワン'
-      when diff >= 2
-        "#{diff}ボギー"
-      else
-        SCORE_MAPPING[diff]
-    end
+              # ホールインワンよりコンドルを優先する
+              when score == 1 && diff != -4
+                'ホールインワン'
+              when diff >= 2
+                "#{diff}ボギー"
+              else
+                SCORE_MAPPING[diff]
+              end
   end
 
   puts result.join(',')
 end
-
 
 # text_case.txtに書いてある2つの配列を標準入力で読み込んで配列にいれる
 pars = gets.chomp.split(',').map(&:to_i)
